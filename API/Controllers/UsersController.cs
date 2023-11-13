4f9293117205771f4a16c419dd17f42dd85c1f81
Damiano <135213161+damidl1@users.noreply.github.com>
2023-11-13 17:53:36 +0100

    auth services and token

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,13 +1,13 @@
 ﻿using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]  // /api/users
-public class UsersController : ControllerBase
+[Authorize]
+public class UsersController : BaseApiController
 {
     private readonly DataContext _context;
     public UsersController(DataContext context)
@@ -15,6 +15,7 @@ public class UsersController : ControllerBase
         _context = context;
     }
 
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
@@ -23,6 +24,7 @@ public class UsersController : ControllerBase
         return users;
     }
 
+
     [HttpGet("{id}")] // /api/users/2
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
@@ -30,4 +32,5 @@ public class UsersController : ControllerBase
         return await _context.Users.FindAsync(id);
     
     }
+
 }
