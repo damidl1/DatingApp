e2e9cfb19bffa0f00577ff3c6806a77e6f27a74e
Damiano <135213161+damidl1@users.noreply.github.com>
2023-11-20 15:20:01 +0100

    auth bug

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,33 +4,35 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers;
-
-[Authorize]
-public class UsersController : BaseApiController
+namespace API.Controllers
 {
-    private readonly DataContext _context;
-    public UsersController(DataContext context)
+
+    [Authorize]
+    public class UsersController : BaseApiController
     {
-        _context = context;
+
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+
+            return users;
+        }
+
+
+        [HttpGet("{id}")] // /api/users/2
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+
+            return await _context.Users.FindAsync(id);
+
+        }
     }
-
-    [AllowAnonymous]
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-    {
-        var users = await _context.Users.ToListAsync();
-
-        return users;
-    }
-
-
-    [HttpGet("{id}")] // /api/users/2
-    public async Task<ActionResult<AppUser>> GetUser(int id)
-    {
-       
-        return await _context.Users.FindAsync(id);
-    
-    }
-
 }
